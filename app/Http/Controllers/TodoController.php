3e05effb3f0fa4e47a5d14e8729e745c87191ce9
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-08-05 22:38:13 +0300

    finish todo api

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string',
+            'completed' => 'required|boolean'
+        ]);
+
+        $todo = Todo::create($data);
+
+        return response($todo, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        $data = Todo::find($todo);
+        return resposnve($data, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $data = $request->validate([
+            'title' => 'required|string',
+            'completed' => 'required|boolean'
+        ]);
+
+        $todo->update($data);
+
+        return response($todo, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response('Item deleted', 200);
+    }
+}
